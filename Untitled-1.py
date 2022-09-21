86a5a44c41cc3bccb2b871138b69a1016213aef6
Dawid Dymke <dawid584@yahoo.com>
2022-09-21 11:12:41 +0200

    fix file

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,106 +1,104 @@
 import random
+from turtle import title
+from typing import ItemsView
 
-class movie:
+class Movie:
     def __init__(self , title ,publish , type ,play_counter) -> None:
         self.title = title
         self.publish = publish
         self.type = type
-        self.play_counter = play_counter 
+        self.play_counter = play_counter = 0
     
     def __repr__(self):
         return f"{self.title}  {self.publish}  {self.type} {self.play_counter}"
     
     
-    
-    def generate_views(self):
-        play_counter = play_counter * 10
+    def play(self ):
+        self.play_counter += 1
         
-    
-    def play(self,play_counter = 50):
-        play_counter += 1
-        return f"{self.title}  {self.publish}  {self.type} {play_counter}"
-    
-    
-class series(movie):
+  
+    def generate_views(self ):
+        self.play_counter *=10
+  
+class Series(Movie):
     def __init__(self , title , publish , type , play_counter , sezon , episode):
         super().__init__( title , publish , type , play_counter )
         self.sezon = sezon
         self.episode = episode
     def __repr__(self):
         return f" {self.title} {self.publish}  {self.type} {self.play_counter} {self.sezon} {self.episode} "
-    
-    
 
-
-class library:
+class Library:
 
     def __init__(self):
         self.library =[]
         
 
     def add(self, object):
-        if not (type(object) == movie or type(object) == series): raise ValueError("To musi być film lub serial")
+        
+        if not (type(object) == Movie or type(object) == Series): raise ValueError("To musi być film lub serial")
         self.library.append(object)
-       
+     
         
     def get_movies(self):
-          return [i for i in self.library if type(i) == movie]         
+          return [i for i in self.library if type(i) == Movie]         
     
     def get_series(self):
-          return [i for i in self.library if type(i) == series]
+          return [i for i in self.library if type(i) == Series]
     
-   
+    def generate_views(self):
+        k = random.choice(self.library)
+        liczba = random.randint(0,100)   
+        k.play_counter = liczba
+       
+    def input_movie(self , name): 
+        self.name = name
 
-m = movie("Annabelle" , 2019 , "Horror" , 5*10)
-s = series("The walking dead", 2022 , "Horror", 10*10 , "E02" ,"S11")
-x = library()
+    def search(self):
+        
+        Lista_1 = []  
+        e = self.library
+        
+        for i in self.library:
+           Lista_1.append(i.title)
+        for b in Lista_1:
+            if b == self.name:
+                return self.name  
+
+    def input_amounts(self, amount):
+        self.amount = amount
+    
+    def top_title(self):
+        list_1 = [] 
+                
+        dictionary ={}
+        for i in self.library:
+           dictionary[i.play_counter] = i.title 
+                       
+        dictionary = sorted(dictionary.items() , reverse=True )
+        
+        
+
+        for i in range(0 , self.amount):
+            list_1.append(dictionary[i])
+           
+        return f' Najpopularniejsze tytuły oraz ilośc wyświetleń: {list_1}'
+
+m = Movie("Annabelle" , 2019 , "Horror" , 0)
+s = Series("The walking dead", 2022 , "Horror", 0 , "E02" ,"S11")
+x = Library()
 x.add(m)
 x.add(s)
-
-
-
-play = movie("Annabelle" , 2019 , "Horror" , 5)
-
-play.play()
-
-t = input("Wpisz nazwe serialu")
-
-def search(t):
-    if t == m.title:
-       print(f' Wyszukany film: {x.get_movies()}')
-    elif t == s.title:
-       print(f' Wyszukany serial: {x.get_series()}')
-    else:
-       print("Brak filmu o podanym tytule")
-search(t)    
-
+x.generate_views()
+s.generate_views()
+m.generate_views()
+s.play()
+x.input_movie("Annabelle")
+x.search()
+x.input_amounts(int(1))
+x.top_title()
 
 print(f'Film: {x.get_movies()}')
 print(f'Serial: {x.get_series()}')
-print(f' Odtwarzany film: {play.play()}')
-
-
-
-    
-def top_titles():
-    if m.play_counter > s.play_counter: 
-        
-        print (f'Najczęściej oglądany film: {m}')
-        
-    else:              
-        print (f'Najczęściej oglądany film:{s}')
-top_titles()        
-
-
-
-
-def generate_views():
-       z = random.choice([x.library[0] , x.library[1]])
-       if z == x.library[1]:
-            z = movie("Annabelle" , 2019 , "Horror" , random.randint(0, 100))
-            print(f'Losowa ilośc wyświetleń od 0-100 {z}')
-       else:   
-            z = series("The walking dead", 2022 , "Horror", random.randint(0, 100) , "E02" ,"S11")
-            print(f'Losowa ilośc wyświetleń od 0-100 {z}')
-        
-generate_views()       
+print(f'Film istnieje: {x.search()}')
+print(x.top_title())
